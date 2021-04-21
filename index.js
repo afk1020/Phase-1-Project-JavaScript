5ec7e53c11188e9dbf998a10c74ef5af60c2927d
Ashley Knorr Gehring <ashleyknorr@gmail.com>
2021-04-21 13:33:45 -0500

    working mvp version

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=2XLVBt8SC09b9rI2
         console.log(nasaData)
 
         document.querySelector('.title').innerText = nasaData.title;
-        //document.querySelector('.image').src = nasaData.hdurl;
         if(nasaData.media_type == "image"){
              document.querySelector('.image').src = nasaData.hdurl;
         }
@@ -26,108 +25,97 @@ fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=2XLVBt8SC09b9rI2
         })
 }      
 
+
+const localURL ="http://localhost:3000/images"
+fetch(localURL)
+.then(res => res.json())
+.then(localData => {renderData(localData)
+    console.log(localData)})
+
+function renderData(localData){
     
-
-
-// const localURL ="http://localhost:3000/images"
-// fetch(localURL)
-// .then(res => res.json())
-// .then(localData => {renderData(localData)
-//     console.log(localData)})
-
-// function renderData(localData){
-    
-//      document.querySelector(".date-form").addEventListener('submit',()=>{
-//       event.preventDefault()  
-//       console.log(event.target)
+     document.querySelector(".date-form").addEventListener('submit',()=>{
+      event.preventDefault()  
+      console.log(event.target)
      
     
-//      let newDate = {
-//         "date":event.target.name.value,
-//         "explanation": event.target.name.value,
-//         "stars": "0",
-//         "image": event.target.name.value
-//     }
-//         console.log(newDate)
+     let newDate = {
+        "date":event.target.name.value,
+        "explanation": event.target.name.value,
+        "stars": "0",
+        "image": event.target.name.value
+    }
+        console.log(newDate)
      
 
-// document.querySelector(".date-2").innerText = localData[2].date
-// console.log(localData[1].date)
-// document.querySelector(".image").src = localData[2].image
-// document.querySelector(".explanation").innerText =localData[2].explanation   
-
-
-//      let stars = document.querySelector(".stars")
-//      stars.innerText = localData[0].stars +" stars"
+     let stars = document.querySelector(".stars")
+     stars.innerText = localData[0].stars +" stars"
+     // needs to be dynamic
    
      
 
-//     document.querySelector(".stars-button").addEventListener("click",()=>{
-//         let count = parseInt(stars.innerText)
-//         ++ count
-//         stars.innerText = count + " stars"
+    document.querySelector(".stars-button").addEventListener("click",()=>{
+        let count = parseInt(stars.innerText)
+        ++ count
+        stars.innerText = count + " stars"
     
-//     let starsUpdate ={
-//         "stars":count
-//     };
-//     patchImage(starsUpdate);
+    let starsUpdate ={
+        "stars":count
+    };
+    patchImage(starsUpdate);
 
-//     })
-//     document.querySelector(".comment-form").addEventListener('submit',()=>{
-//         event.preventDefault()
-//         let newComment = document.querySelector(".comment-input").value
-//         const ul = document.querySelector(".comments")
-//         const li = document.createElement('li')
-//         const text = document.createTextNode(newComment)
-//         li.append(text)
-//         ul.append(li)
-// console.log("works")
-//     patchComment(newComment)
-//     })
-// })
-// }
+    })
+    document.querySelector(".comment-form").addEventListener('submit',()=>{
+        event.preventDefault()
+        let newComment = document.querySelector(".comment-input").value
+        const ul = document.querySelector(".comments")
+        const li = document.createElement('li')
+        const text = document.createTextNode(newComment)
+        li.append(text)
+        ul.append(li)
+console.log("works")
+    patchComment(newComment)
+    })
+})
+}
 
 
-// function postImage(localData){
+function postImage(localData){
     
-//     newURL2 = "http://localhost:3000/comments/1" 
-//     let optionsPost={
-//         method: "POST",
-//         headers:{
-//             "Content-Type": "application/json",
-//             Accept: "application/json",
-//         },
-//         body: JSON.stringify(localData)
-//         };
+    newURL2 = "http://localhost:3000/comments/1" 
+    let optionsPost={
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(localData)
+        };
 
-// fetch(newURL2, optionsPost);
-// };
-// function patchImage(starsUpdate){
-//     newURL = "http://localhost:3000/images/1" //changed url to feature id of 1
-//     let optionPatch ={
-//         method: 'PATCH',
-//         headers: {
-//             "Content-Type":"application/json",
-//             Accept: "application/json",
-//         },
-//         body: JSON.stringify(starsUpdate)
-//     }
-//     fetch(newURL, optionPatch);
-// };
-// function patchComment(newComment){
-//     newURL = "http://localhost:3000/comments/" //changed url to feature id of 1
-//     let optionPatch ={
-//         method: 'PATCH',
-//         headers: {
-//             "Content-Type":"application/json",
-//             Accept: "application/json",
-//         },
-//         body: JSON.stringify(newComment)
-//     }
-//     fetch(newURL, optionPatch);
-// }
+fetch(newURL2, optionsPost);
+};
+function patchImage(starsUpdate){
+    newURL = "http://localhost:3000/images/1" //changed url to feature id of 1, needs to be dynamic
+    let optionPatch ={
+        method: 'PATCH',
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(starsUpdate)
+    }
+    fetch(newURL, optionPatch);
+};
+function patchComment(newComment){
+    newURL = "http://localhost:3000/comments/" //changed url to feature id of 1, needs to be dynamic
+    let optionPatch ={
+        method: 'PATCH',
+        headers: {
+            "Content-Type":"application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(newComment)
+    }
+    fetch(newURL, optionPatch);
+ }
 
-//const nasaURL =  "https://api.nasa.gov/planetary/apod?api_key=2XLVBt8SC09b9rI2nxK0qwhhGw9kCt1e9ZoTaJc9"
-// fetch(nasaURL)
-// .then((res)=> res.json())
-// .then(handleData => handleData)
